64b048afb2b7cb5664ad39e99739868d458f834d
Paweł Polakiewicz <pp.polakiewicz@gmail.com>
2026-05-02 21:41:50 +0200

    chore: Add TestContainers and Spock for testing

--- backend/src/test/java/com/shootersplatform/backend/ShootersPlatformApplicationTests.java
+++ backend/src/test/java/com/shootersplatform/backend/ShootersPlatformApplicationTests.java
@@ -15,7 +15,7 @@ class ShootersPlatformApplicationTests {
 
     @Container
     @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18.3-alpine");
 
     @Test
     void contextLoads() {
